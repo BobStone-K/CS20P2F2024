d03919c9fcec54f9a8c59ae6fa259b59d5d7bcb8
38262004 <38262004@L815WD220011.edu.cbe.ab.ca>
2024-09-20 10:47:13 -0600

    Waddup

--- Chapter3/src/Mastery/election.java
+++ Chapter3/src/Mastery/election.java
@@ -16,17 +16,23 @@ public class election {
 		int trumpTotVotes, kamalaTotVotes, totVotes;
 		double trumpPer, kamalaPer;
 		//prompting user for votes in each state and recording them
-		System.out.print("Please enter amount of Votes Trump got in New York: ");
+		//NewYork section
+		System.out.println("Election results for New York:");
+		System.out.print("Trump: ");
 		trumpNYVotes = Input.nextInt();
-		System.out.print("Please enter amount of Votes Kamala got in New York: ");
+		System.out.print("Kamala: ");
 		kamalaNYVotes = Input.nextInt();
-		System.out.print("Please enter amount of Votes Trump got in New Jersey: ");
+		//NewJersey section
+		System.out.println("Election results for New Jersey:");
+		System.out.print("Trump: ");
 		trumpNJVotes = Input.nextInt();
-		System.out.print("Please enter amount of Votes Kamala got in New Jersey: ");
+		System.out.print("Kamala: ");
 		kamalaNJVotes = Input.nextInt();
-		System.out.print("Please enter amount of Votes Trump got in Connecticut: ");
+		//Connecticut section
+		System.out.println("Election results for Conneticut:");
+		System.out.print("Trump: ");
 		trumpConVotes = Input.nextInt();
-		System.out.print("Please enter amount of Votes Kamala got in Connecticut: ");
+		System.out.print("Kamala: ");
 		kamalaConVotes = Input.nextInt();
 		//add up all the votes to get each candidates total from the three states
 		trumpTotVotes = trumpNYVotes + trumpNJVotes + trumpConVotes;
@@ -36,16 +42,6 @@ public class election {
 		//get the percentage of votes each candidate got out of total votes
 		trumpPer = (Double.valueOf(trumpTotVotes)/Double.valueOf(totVotes)) * 100;
 		kamalaPer = (Double.valueOf(kamalaTotVotes)/Double.valueOf(totVotes)) * 100 ;
-		//outputting each candidates total votes, their percentage, and the total votes
-		System.out.println("Election results for New York:");
-		System.out.println("Trump: " + trumpNYVotes);
-		System.out.println("Kamala: " + kamalaNYVotes);
-		System.out.println("Election results for New Jersey:");
-		System.out.println("Trump: " + trumpNJVotes);
-		System.out.println("Kamala: " + kamalaNJVotes);
-		System.out.println("Election results for Conneticut:");
-		System.out.println("Trump: " + trumpConVotes);
-		System.out.println("Kamala: " + kamalaConVotes);
 		//outputting each candidates total votes, their percentage, and the total votes
 		System.out.println("Candidate     Votes  Percentage");
 		System.out.println("Trump:        " + trumpTotVotes + "    "+ formatter.format(trumpPer) + "%");
