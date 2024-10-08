027670bc6659e4a4e32ff9d65ab4b1b47b58fbd3
38262004 <38262004@L815WD220011.edu.cbe.ab.ca>
2024-10-08 10:13:25 -0600

    waddup

--- Chapter5/src/Mastery/GroupAssignment.java
+++ Chapter5/src/Mastery/GroupAssignment.java
@@ -10,23 +10,23 @@ public class GroupAssignment {
 	     Scanner Input = new Scanner(System.in); 
 	     //declaring variables
 	     String lastName, firstName;
-	     char Initial;
+	     char initial;
 	     int groupNum = 0;
 	     //prompting user for their first and last name and recording them
 	     System.out.print("Enter your first name: ");
 	     firstName = Input.next();
-	     System.out.print("Enter your first name: ");
+	     System.out.print("Enter your last name: ");
 	     lastName = Input.next();
 	     //taking first initial
-	     Initial = Character.toUpperCase(lastName.charAt(0));
+	     initial = Character.toUpperCase(lastName.charAt(0));
 	     //checking to see what group, user falls into
-	     if (Initial >= 'A' && Initial <= 'I') {
+	     if (initial >= 'A' && initial <= 'I') {
 	    	 //if first letter of last name is between A and I then in group 1
 	    	 groupNum = 1;}
-	      else if (Initial >= 'J' && Initial <= 'S') {
+	      else if (initial >= 'J' && initial <= 'S') {
 	    	//if first letter of last name is between J and S then in group 2
 	    	 groupNum = 2;}
-	      else if (Initial >= 'T' && Initial <= 'Z') {
+	      else if (initial >= 'T' && initial <= 'Z') {
 	    	//if first letter of last name is between T and Z then in group 3
 	    	  groupNum = 3;}
 	     //Outputs user's full name and what group they are in
