26b67f48410f995b546bbfd15443b9b602b9ad81
38262004 <38262004@L815WD220011.edu.cbe.ab.ca>
2024-12-10 10:25:29 -0700

    11am

--- Chapter9/src/Mastery/CourseGrades.java
+++ Chapter9/src/Mastery/CourseGrades.java
@@ -1,10 +1,8 @@
 /*
 
-Program: ????.java          Last Date of this Revision: December 7, 2024
+Program: CourseGrades.java          Last Date of this Revision: December 7, 2024
 
-Purpose: An application that uses nested for loops to output a table that diplays number in 
- multiples of ten.
-
+Purpose: tests GradeBook class.
 Author: Youdis Acharya, 
 School: CHHS
 Course: Computer Science 20
@@ -17,12 +15,23 @@ public class CourseGrades {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//creating scanner object to record inputs
 		Scanner input = new Scanner(System.in);
+		//Creating GradeBook object
 		GradeBook stuGrades = new GradeBook();
+		//using getGrades method to prompt the user for the 5 test grades for each of the 12 students
 		stuGrades.getGrades();
+		//showgGrades method to display the grades of all the students
 		stuGrades.showGrades();
+		//prompting user for which student's average they want to see
 		System.out.print("Please enter student whose average you want: ");
-		System.out.print(""stuGrades.studentAvg(input.nextInt()));
+		//putting users answer as parameter for studentAvg method, then will print the return statement of that method as the students's average
+		System.out.print("Their average is: " + stuGrades.studentAvg(input.nextInt())+ "%");
+		System.out.println("");
+		//prompting user for which test's average they want to see
+		System.out.print("Please which test's average you want to see: ");
+		//putting users answer as parameter for testAvg method, then will print the return statement of that method as the test's average
+		System.out.println("The average is: " + stuGrades.testAvg(input.nextInt()) + "%");
 		
 	}
 
--- Chapter9/src/Mastery/GradeBook.java
+++ Chapter9/src/Mastery/GradeBook.java
@@ -1,3 +1,13 @@
+/*
+Program: CourseGrades.java          Last Date of this Revision: December 7, 2024
+
+Purpose: Create a CourseGrades application that simulates a grade book for a class with 12 students that each have
+5 test scores.
+Author: Youdis Acharya, 
+School: CHHS
+Course: Computer Science 20
+*/
+
 package Mastery;
 import java.util.Scanner;
 public class GradeBook {
@@ -20,16 +30,17 @@ public class GradeBook {
 			
 		}
 	}
+	//loops through every student's array of 5 test grades and outputs them below an output statement of the student whos grades are being displayed
 	public void showGrades() {
 		for(int student = 0; student< grades.length; student++) {
 			System.out.println("Student " + (student + 1));
 			for (int test = 0; test< grades[test].length; test++) {
-				System.out.println("Grade for test " + (test + 1) + ": " + grades[student][test] );
+				System.out.println("Grade for test " + (test + 1) + ": " + grades[student][test] + "%");
 			}
 		
 		}
 	}
-	
+	//Takes parameter of student num to indicate which student it is then adds up all their tests grades and returns that sum divided by 5 which is their average
 	public double studentAvg(int studentNum) {
 		double avg = 0;
 		for (int test = 0; test<grades[studentNum].length; test++) {
@@ -38,6 +49,14 @@ public class GradeBook {
 		return (avg/5);
 			
 		
+	}
+	//Takes parameter of which test number then adds all of the students grades on that test and divides by 12 to get the average and returns that quotient 
+	public double testAvg(int testNum) {
+		int Avg = 0;
+		for(int student = 0; student < grades.length; student++) {
+			Avg =+ grades[student][testNum-1];
+		}
+		return (Avg/12);
 	}
 	
 	
