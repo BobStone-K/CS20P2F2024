0b0d1175c1d736fa54dd0ef5b83cea6710f8ead5
38262004 <38262004@L815WD220011.edu.cbe.ab.ca>
2025-01-06 11:54:23 -0700

    11am

--- GettingStarted/src/ReadTemperature.java
+++ GettingStarted/src/ReadTemperature.java
@@ -1,9 +1,20 @@
-
+import com.phidget22.*;
 public class ReadTemperature {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	 public static void main(String[] args) throws Exception{
 
-	}
+        //Create 
+        TemperatureSensor temperatureSensor = new TemperatureSensor();
+
+        //Open 
+        temperatureSensor.open(1000);
+
+        //Use your Phidgets 
+        while (true) {
+            System.out.println("Temperature in Farenheit: " + (1.8*temperatureSensor.getTemperature() + 32) + "°F" );
+            System.out.println("Temperature in Celsius: " + (1.8*temperatureSensor.getTemperature()) + "°C" );
+            Thread.sleep(150);
+	        }
+	    }
 
 }
--- GettingStarted/src/ReadTemperatureAndHumidity.java
+++ GettingStarted/src/ReadTemperatureAndHumidity.java
@@ -1,9 +1,36 @@
-
+import com.phidget22.*;
 public class ReadTemperatureAndHumidity {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) throws Exception{
 
-	}
+        //Create 
+        HumiditySensor humiditySensor = new HumiditySensor();
+        TemperatureSensor temperatureSensor = new TemperatureSensor();
+
+        //Open 
+        humiditySensor.open(1000);
+        temperatureSensor.open(1000);
+
+        //Use your Phidgets 
+        while(true){
+        	if (humiditySensor.getHumidity() < 30) {
+        		System.out.println("Humidity is low");
+        		Thread.sleep(1000);
+        	}
+        	else {
+        		System.out.println("Humidity: " + humiditySensor.getHumidity() +" %RH ");
+        		Thread.sleep(200);
+        	}
+        	
+        	if (temperatureSensor.getTemperature() < 21) {
+        		System.out.println("Room is too cold.");
+	            Thread.sleep(1000);
+        	}
+            else {
+                System.out.println(" Temperature: " + temperatureSensor.getTemperature() + " °C");
+                Thread.sleep(200);
+            }
+        }
+    }
 
 }
